854b504266b9e4aa20cc05cdb30bd3895eac8976
1xo2 <yanivsfreelance@hotmail.com>
2024-06-19 09:59:55 +0200

    fixing remote build

--- client/src/pages/__test__/HomePage.test.tsx
+++ client/src/pages/__test__/HomePage.test.tsx
@@ -1,16 +1,21 @@
 import { cleanup, render, screen } from "@testing-library/react";
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import HomePage from "../home/HomePage";
+import { BrowserRouter } from "react-router-dom";
 
 
 afterEach(() => {
   cleanup();
 });
 beforeEach(() => {
-  render(<HomePage />);
+  render(
+    <BrowserRouter>
+      <HomePage />
+    </BrowserRouter>
+  );
 });
 describe("AboutPage", () => {
   it("should render page correctly", () => {
-    expect(screen.getByText("HomePage")).toBeDefined();
+    expect(screen.getByText("perfect")).toBeDefined();
   });
 });
