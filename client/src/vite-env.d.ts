57c7afd120b36408c37848e7a1347eaaf6a37e56
1xo2 <yanivsfreelance@hotmail.com>
2024-06-25 17:20:13 +0200

    client vite config

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -2,9 +2,10 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
+  VITE_APP_API_ENDPOINT: string;
   // more env variables...
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;  
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  // mode: process.env.NODE_ENV,
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      // each time you see "/api" place the target
-      "/api": {
-        target: "http://localhost:8000",
-        // for not using SSL (in prod)
-        secure: false, //isProduction,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: mode === 'development' ? 'http://localhost:8000' : 'https://real-estate-server-bs48.onrender.com/',//import.meta.env.VITE_APP_API_ENDPOINT,
+          secure: false, // Adjust this based on your security requirements
+        },
       },
     },
-  },
+    build: {
+      sourcemap: mode === 'development',
+    },
+  };
 });
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   // mode: process.env.NODE_ENV,
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       // each time you see "/api" place the target
+//       "/api": {
+//         target: "http://localhost:8000",
+//         // for not using SSL (in prod)
+//         secure: false, //isProduction,
+//       },
+//     },
+//   },
+// });
